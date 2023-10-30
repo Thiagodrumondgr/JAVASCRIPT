90bb3520bd2ec06a71ba841e44afd1d1b1ae563f
ThiagoDrumond <137659083+Thiagodrumondgr@users.noreply.github.com>
2023-10-30 11:33:13 -0300

    INICIANDO ESTUDOS JS

---
+++ EX001/index.js
@@ -0,0 +1,1 @@
+console.log('Meu nome é "Thiago Drumond", Estou aprendendo Javascript ás', 10 ,'da manhã');
---
+++ index.js
@@ -0,0 +1,5 @@
+console.log('thiago "drumond"'); //String = texto
+console.log("thiago 'drumond'"); //number = Número
+console.log(`'thiago' "drumond"`);
+
+console.log(126, 154.55, 'thiago drumond');
