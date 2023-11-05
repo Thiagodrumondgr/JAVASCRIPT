9c7f2971b24c1be6b61d49ce334a5115955162b6
ThiagoDrumond <137659083+Thiagodrumondgr@users.noreply.github.com>
2023-11-05 18:17:12 -0300

    atualização aula

---
+++ EX013/index.js
@@ -0,0 +1,10 @@
+console.log(100/0)
+//let num1 = 25;
+//console.log(num1 ** 0.5); (faz a raiz quadrada de um numero)
+//let num2 = Math.floor(num1); (ARREDONDA NUMERO PARA BAIXO)
+//let num2 = Math.ceil(num1); (ARREDONDA NUMERO PARA CIMA)
+//let num2 = Math.round(num1); (ARREDONDA PARA O NUMERO MAIS PROXIMO,PASSANDO DA METADE ARREDONDA PARA CIMA )
+//console.log(Math.max(1, 2, 3, 9, -10, 58, -2, 1500, 5, 154)); (ACHA O MAIOR NUMERO DENTRE ESSES ESCRITOS)
+//console.log(Math.min(1,2,3,6,150,875,-1,-50,-450,4585,-1500)) (ACHA O MENOR NUMERO DENTRO OS DIGITADOS)
+//const aleatorio =Math.round (Math.random() * (10-5) + 5) (ESCOLHE UM NUMERO ALEATORIO ENTRE 5 E 10 COMO FOI PEDIDO)
+//console.log(aleatorio);
